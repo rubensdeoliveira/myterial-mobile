e360f0692586169117049a50f6be761e00c12e52
rubensojunior <rubensojunior6@gmail.com>
2020-07-17 00:39:42 -0300

    create dashboard and implementing navigation to dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import api from '../../services/api'
+import { useAuth } from '../../hooks/auth'
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  Username,
+  ProfileButton,
+  UserAvatar,
+} from './styles'
+
+const Dashboard: React.FC = () => {
+  const { user } = useAuth()
+  const { navigate } = useNavigation()
+
+  useEffect(() => {
+    api.get('materials').then((response) => {
+      console.log(response.data)
+    })
+  }, [])
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile')
+  }, [navigate])
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <Username>{user.name}</Username>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+    </Container>
+  )
+}
+
+export default Dashboard
