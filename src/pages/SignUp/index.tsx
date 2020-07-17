46eb7398074a2a3876e2fac58f30788edc6567ff
rubensojunior <rubensojunior6@gmail.com>
2020-07-17 00:35:01 -0300

    Implementing sign up page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,153 @@
-import React from 'react'
+import React, { useRef, useCallback } from 'react'
+import {
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  TextInput,
+  Alert,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
+import * as Yup from 'yup'
+import getValidationErrors from '../../utils/getValidationErrors'
+import api from '../../services/api'
 
-import { Container } from './styles'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { Container, Title, BackToSign, BackToSignText } from './styles'
+
+interface SignUpFormData {
+  name: string
+  email: string
+  password: string
+}
 
 const SignUp: React.FC = () => {
-  return <Container />
+  const formRef = useRef<FormHandles>(null)
+  const navigation = useNavigation()
+
+  const emailInputRef = useRef<TextInput>(null)
+  const passwordInputRef = useRef<TextInput>(null)
+
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({})
+
+        const schema = Yup.object().shape({
+          name: Yup.string().min(6, 'Nome deve ter no mínimo 6 dígitos'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('E-mail inválido'),
+          password: Yup.string().min(6, 'Senha deve ter no mínimo 6 dígitos'),
+        })
+
+        await schema.validate(data, {
+          abortEarly: false,
+        })
+
+        await api.post('/users', data)
+
+        Alert.alert(
+          'Cadastro realizado com sucesso!',
+          'Entre com suas informações para continuar',
+        )
+
+        navigation.goBack()
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err)
+
+          formRef.current?.setErrors(errors)
+
+          return
+        }
+
+        Alert.alert('Erro no cadastro', err.message)
+      }
+    },
+    [navigation],
+  )
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Form ref={formRef} onSubmit={handleSignUp}>
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus()
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus()
+                }}
+              />
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm()
+                }}
+              />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm()
+                }}
+              >
+                Cadastrar
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSign
+        onPress={() => {
+          navigation.navigate('SignIn')
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignText>Voltar para logon</BackToSignText>
+      </BackToSign>
+    </>
+  )
 }
 
 export default SignUp
